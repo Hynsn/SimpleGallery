8523334ca7b8936d14b05362a01b851e46d34028
Hynsn <hynson@163.com>
2021-09-13 00:25:15 +0800

    code marge

--- kt_learn/src/main/java/com/hynson/learn/DSL.kt
+++ kt_learn/src/main/java/com/hynson/learn/DSL.kt
@@ -1,10 +1,5 @@
 package com.hynson.learn
 
-class View{
-
-
-}
-
 class Button(){
 
 }
@@ -23,42 +18,53 @@ class Layout{
 
 data class TempTime(val temp: Int, val time:Int)
 
-class Param(name:String, resId:Int, mod: String, recipe:Int, tt: TempTime, shake:Boolean){
+class Param(val name:String,val resId:Int, val mod: String, val recipe:Int,val tt: TempTime,val shake:Boolean){
 
+    override fun toString(): String {
+        return "Param(name='$name')"
+    }
 }
 
 class Product(val configMod:String){
+    private lateinit var pparams:List<Param>
 
     companion object {
         val map = HashMap<String,Product>()
-
-        val productMap = HashMap<String,Param>()
     }
 
     init {
-        map.put(configMod,this)
+
     }
 
-    fun add(action:Param.() -> Unit){
-        action.let {
-            println("add-->$it")
-        }
-        map.forEach {
-            println("it-->${it.value} ${System.identityHashCode(map)}")
-        }
-    }
-    fun add2(param: Param): Product{
-        productMap.put(configMod,param)
-
+    fun init(action:Product.() -> Unit): Product{
+        action.invoke(this)
         return this
+    }
+
+    fun add(vararg params: Param){
+        pparams = params.toList()
+
+        map[configMod] = this
     }
 
     override fun toString(): String {
         return "Product(configMod='$configMod')"
     }
 
+    fun printMap(){
+        map.forEach {
+            println(it.value.pparams)
+        }
+    }
+
 }
 
+/**
+ * 1.根据config取出所有预设参数
+ * 2.根据Mode取出对应的name icon
+ *
+ *
+ */
 class Builder(){
     fun get(key:String):String?{
 //        map[configMod] = configMod
@@ -66,6 +72,8 @@ class Builder(){
 
         return null
     }
+
+
 }
 
 fun build(lambda:Builder.() -> Unit) : Builder{
@@ -103,17 +111,53 @@ fun main(){
     }
     // infax 表达式必须是成对的
 
+
+    /*
+    协程解决的问题，使用同步思维达到异步的效果
+
+
+    串行思维
+
+    解决接口过多回调嵌套的情况
+
+    suspend 挂起函数，可能会执行异常操作调用需要传递，
+
+
+
+
+
+协程原理
+3个点
+首次执行传入续体和上下文
+with resume
+
+挂起枚举状态
+
+调用机传递，最终会调用invoke suspend 以此类推最后跳出循环。
+
+suspend就是一个提醒功能，需要传递
+怎么让多个协程同时执行，最后再一起执行？
+async{}.await()
+     */
+
     val builder = build {
-        Product("DS03").add {
-            Param("test",12,mod = "测试",24,TempTime(12,34),true)
-            Param("test",12,mod = "测试",24,TempTime(12,34),true)
-        }
+        Product("DS03").init {
+            val tempTime = TempTime(12,34)
+            add(
+                Param("test",12,mod = "M1",24,tempTime,true),
+                Param("test",12,mod = "M2",24,tempTime,true)
+            )
+        }.printMap()
+
         Product("DS04")
-            .add2(Param("test",12,mod = "测试",24,TempTime(12,34),true))
-            .add2(Param("test",12,mod = "测试",24,TempTime(12,34),true))
-        Product("DS06").add {
-            Param("test",12,mod = "测试",24,TempTime(12,34),true)
-            Param("test",12,mod = "测试",24,TempTime(12,34),true)
-        }
+            .add(
+                Param("test",12,mod = "M3",24,TempTime(12,34),true),
+                Param("test",12,mod = "M4",24,TempTime(12,34),true)
+            )
+        Product("DS06")
+            .add(
+                Param("test",12,mod = "M5",24,TempTime(12,34),true),
+                Param("test",12,mod = "M6",24,TempTime(12,34),true)
+        )
     }
 }
