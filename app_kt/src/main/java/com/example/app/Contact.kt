d1b228d896135404b836d0b13b6b239bb4971bf6
Hynsn <hynson@163.com>
2022-10-21 08:36:14 +0800

    列表使用

--- app_kt/src/main/java/com/example/app/Contact.kt
+++ app_kt/src/main/java/com/example/app/Contact.kt
@@ -1,9 +1,12 @@
 package com.example.app
 
+import androidx.compose.runtime.MutableState
 import androidx.compose.ui.graphics.Color
+import kotlinx.coroutines.flow.MutableStateFlow
 
 data class Contact(
+    val type:Int,
     val userIcon: Color,
     val userName:String,
-    val email:String
+    var email: MutableState<String>
 )
--- app_kt/src/main/java/com/example/app/MainActivity.kt
+++ app_kt/src/main/java/com/example/app/MainActivity.kt
@@ -8,22 +8,20 @@ import android.view.MotionEvent
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Canvas
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
+import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.CutCornerShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateListOf
-import androidx.compose.runtime.mutableStateOf
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.*
+import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.snapshots.SnapshotStateList
 import androidx.compose.ui.Alignment
@@ -40,80 +38,214 @@ import androidx.compose.ui.graphics.drawscope.rotate
 import androidx.compose.ui.input.pointer.pointerInteropFilter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalTextToolbar
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.PreviewParameter
 import androidx.compose.ui.tooling.preview.PreviewParameterProvider
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.compose.ui.window.DialogProperties
+import androidx.compose.ui.window.SecureFlagPolicy
 import com.example.app.ui.theme.SimpleGalleryTheme
 import com.google.accompanist.glide.rememberGlidePainter
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     @ExperimentalComposeUiApi
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Log.i("TAG", "setContent: ${this.javaClass}")
-            SimpleGalleryTheme {
-                Surface(color = MaterialTheme.colors.background) {
-                    Column(
-                        horizontalAlignment = Alignment.Start
-                    ) {
-                        Greeting("Anoi")
-//                        LazyRowItemsDemo()
-//                        DrawView2()
-                        LevelView()
-                    }
+//            Log.i("TAG", "setContent: ${this.javaClass}")
+//            SimpleGalleryTheme {
+//
+//            }
+            Greeting(name = "title", onBack = {
+                finish()
+            })
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, onBack: (() -> Unit)) {
+    val context = LocalContext.current
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(
+                        text = stringResource(id = R.string.device_settings),
+                        modifier = Modifier.fillMaxWidth(),
+                        textAlign = TextAlign.Center,
+                        style = MaterialTheme.typography.h6
+                    )
                 }
+            )
+        },
+    ) {
+        LazyRowItemsDemo(it)
+    }
+}
+
+//    Image(
+//        painter = painterResource(id = R.drawable.ic_launcher_background),
+//        contentDescription = "popular.title",
+//        contentScale = ContentScale.Crop,
+//        modifier = Modifier
+//            .padding(8.dp)
+//            .size(30.dp)
+//            .clip(RoundedCornerShape(corner = CornerSize(16.dp)))
+//    )
+
+@SuppressLint("UnrememberedMutableState")
+@Composable
+fun LazyRowItemsDemo(it: PaddingValues) {
+    val list = mutableStateListOf<Contact>()
+    list.addAll(mockData())
+    val scope = rememberCoroutineScope()
+    val state = rememberLazyListState()
+    val alertDialog = remember { mutableStateOf(false) }
+
+
+//    for (i in 1..100) {
+//        list.add("项目 $i")
+//    }
+    // 指定宽度、高度
+    LazyColumn(
+        modifier = Modifier
+            .padding(8.dp)
+            .fillMaxWidth(),
+        state = state
+    ) {
+//        list.forEach { contact ->
+//            item {
+//                ContactListItem(contact)
+//            }
+//        }
+//        items(list) {
+//            ContactListItem(it)
+//        }
+        val longClicke:(Int)->(Unit) = {
+            alertDialog.value = true
+        }
+        list.forEachIndexed { index, contact ->
+            item {
+                ContactListItem(index, longClicke,contact)
             }
         }
     }
-}
+    showDialog(alertDialog)
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-    Image(
-        painter = painterResource(id = R.drawable.ic_launcher_background),
-        contentDescription = "popular.title",
-        contentScale = ContentScale.Crop,
+
+    //设置靠右下角
+    Column(
+        horizontalAlignment = Alignment.End,
+        verticalArrangement = Arrangement.Bottom,
         modifier = Modifier
-            .padding(8.dp)
-            .size(30.dp)
-            .clip(RoundedCornerShape(corner = CornerSize(16.dp)))
-    )
-}
+            .fillMaxSize()
+            .padding(end = 16.dp, bottom = 16.dp)
+    ) {
+        FloatingActionButton(onClick = {
+            scope.launch {
+                list.removeLast()
+                val index = list.size - 1
+                state.scrollToItem(index)
+            }
 
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    SimpleGalleryTheme {
-        Column(
-            modifier = Modifier.background(Color(0xFFE0FFE3)),
-            horizontalAlignment = Alignment.Start
-        ) {
-            Greeting("Anoi")
-            LazyRowItemsDemo()
+        }) {
+            Icon(
+                imageVector = Icons.Default.ArrowDropDown,
+                contentDescription = null
+            )
+        }
+        FloatingActionButton(onClick = {
+            list.first().email.value = "ddd"
+        }) {
+            Icon(
+                imageVector = Icons.Default.Clear,
+                contentDescription = null
+            )
+        }
+        FloatingActionButton(onClick = {
+            scope.launch {
+                //添加一个新的数据
+                val time = System.currentTimeMillis()
+                list.add(Contact(1, Color.Gray, time.toString(), mutableStateOf("test")))
+                val index = list.size - 1
+                state.scrollToItem(index)
+            }
+
+        }) {
+            Icon(
+                imageVector = Icons.Default.Add,
+                contentDescription = null
+            )
         }
     }
 }
 
+@OptIn(ExperimentalComposeUiApi::class)
 @Composable
-fun LazyRowItemsDemo() {
-    val list = mutableListOf<String>()
-    for (i in 1..100){
-        list.add("项目 $i")
-    }
-    // 指定宽度、高度
-    LazyColumn(modifier = Modifier
-        .padding(8.dp)
-        .fillMaxWidth()) {
-        items(list) {
-            Text(text = it,fontSize = TextUnit.Unspecified)
-        }
+private fun showDialog(alertDialog: MutableState<Boolean>) {
+    if (alertDialog.value) {
+        AlertDialog(
+            //当用户尝试通过单击外部或按下后退按钮来关闭对话框时执行。单击关闭按钮时不会调用此方法。
+            onDismissRequest = {
+                alertDialog.value = false
+            },
+            //对话框的标题，它应该指定对话框的目的。标题不是强制性的，因为文本中可能有足够的信息。提供的文本样式默认为 Typography.h6
+            title = {
+                Text(text = "弹了个框")
+            },
+            //显示有关对话框目的的详细信息的文本。提供的文本样式默认为 Typography.body1
+            text = {
+                Text(
+                    "是否获得屠龙宝刀一把" + "\n" +
+                            "是否获得倚天神剑一把"
+                )
+            },
+            //一个按钮，用于确认的操作，从而解决触发对话框的原因。该对话框不会为此按钮设置任何事件，因此它们需要由调用者设置。null则不显示
+            confirmButton = {
+                TextButton(
+                    onClick = {
+                        alertDialog.value = false
+                    }
+                ) {
+                    Text("确认获取")
+                }
+            },
+            //用于关闭对话框的按钮。该对话框不会为此按钮设置任何事件，因此它们需要由调用者设置,null则不显示
+            dismissButton = {
+                TextButton(
+                    onClick = {
+                        alertDialog.value = false
+                    }
+                ) {
+                    Text("大可不必")
+                }
+            },
+            //对话框的背景颜色
+            backgroundColor = Color.Gray,
+            //此对话框为其子级提供的首选内容颜色。
+            contentColor = Color.Red,
+            //平台特定的属性，以进一步配置对话框
+            properties = DialogProperties(
+                //是否可以通过按下后退按钮来关闭对话框。 如果为 true，按下后退按钮将调用 onDismissRequest。
+                dismissOnBackPress = true,
+                //是否可以通过在对话框边界外单击来关闭对话框。 如果为 true，单击对话框外将调用 onDismissRequest
+                dismissOnClickOutside = true,
+                //用于在对话框窗口上设置 WindowManager.LayoutParams.FLAG_SECURE 的策略。
+                securePolicy = SecureFlagPolicy.Inherit,
+                //对话框内容的宽度是否应限制为平台默认值，小于屏幕宽度。
+                usePlatformDefaultWidth = true
+            )
+        )
     }
 }
 
@@ -137,7 +269,7 @@ fun Content() {
         )
         Column(
             modifier = Modifier.background(Color(0xFFE0FFE3)),
-            horizontalAlignment = Alignment.Start
+            horizontalAlignment = Alignment.End
         ) {
             Row(
                 modifier = Modifier.background(Color(0xFFFFE5E0)),
@@ -237,7 +369,7 @@ fun RowLayout() {
 fun BoxLayout() {
     Box(modifier = Modifier.size(200.dp)) {
         Text(
-            text = "", modifier = Modifier
+            text = "测试2", modifier = Modifier
                 .background(Color(0xFF063142))
                 .fillMaxSize()
         )
@@ -247,7 +379,7 @@ fun BoxLayout() {
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
             Text(
-                text = "",
+                text = "测试",
                 modifier = Modifier
                     .size(
                         100.dp, 100.dp
@@ -258,9 +390,12 @@ fun BoxLayout() {
     }
 }
 
+
+@OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun ContactListItem(
-    @PreviewParameter(ContactDefault::class) contact: Contact
+    index: Int,onLongClick: ((Int) -> Unit)?,
+    @PreviewParameter(ContactDefault::class) contact: Contact,
 ) {
     val context = LocalContext.current
     Row(
@@ -270,14 +405,29 @@ fun ContactListItem(
             .height(60.dp)
             .fillMaxWidth()
             .background(Color.White, RoundedCornerShape(10.dp))
-            .clickable {
+            .combinedClickable(onLongClick = {
+                onLongClick?.invoke(index)
+//                Toast
+//                    .makeText(context, "$index 长按操作", Toast.LENGTH_SHORT)
+//                    .show()
+            }, onDoubleClick = {
                 Toast
-                    .makeText(context, "contact:${contact.toString()}", Toast.LENGTH_SHORT)
+                    .makeText(context, "双击操作", Toast.LENGTH_SHORT)
                     .show()
-            }
+            }, onClick = {
+                Toast
+                    .makeText(context, "点击操作", Toast.LENGTH_SHORT)
+                    .show()
+            })
+//            .clickable {
+//                Toast
+//                    .makeText(context, "contact:${contact.toString()}", Toast.LENGTH_SHORT)
+//                    .show()
+//            }
             .padding(6.dp),
         verticalAlignment = Alignment.CenterVertically
     ) {
+
         Text(
             text = "",
             modifier = Modifier
@@ -286,9 +436,10 @@ fun ContactListItem(
         )
         Column(Modifier.padding(10.dp, 0.dp, 0.dp, 0.dp)) {
             Text(text = contact.userName, fontSize = 16.sp)
-            Text(text = contact.email, fontSize = 10.sp, color = Color(0x52030303))
+            Text(text = contact.email.value, fontSize = 10.sp, color = Color(0x52030303))
         }
     }
+
 }
 
 @Preview
@@ -297,22 +448,22 @@ fun ContactList() {
     val dataSource = mockData()
     SimpleGalleryTheme() {
         LazyColumn() {
-            dataSource.forEach { contact ->
+            dataSource.forEachIndexed { index, contact ->
                 item {
-                    ContactListItem(contact)
+                    ContactListItem(index, null,contact)
                 }
             }
         }
     }
 }
 
-fun mockData() : List<Contact>{
+fun mockData(): List<Contact> {
     val list = mutableListOf<Contact>()
-    list.add(Contact(Color.Cyan,"A","a@163"))
-    list.add(Contact(Color.Black,"B","b@163"))
-    list.add(Contact(Color.Cyan,"C","c@163"))
-    list.add(Contact(Color.Cyan,"D","d@163"))
-    list.add(Contact(Color.Cyan,"E","e@163"))
+    list.add(Contact(1, Color.Cyan, "A", mutableStateOf("a@163")))
+    list.add(Contact(2, Color.Black, "B", mutableStateOf("b@163")))
+    list.add(Contact(3, Color.Cyan, "C", mutableStateOf("c@163")))
+    list.add(Contact(4, Color.Cyan, "D", mutableStateOf("d@163")))
+    list.add(Contact(5, Color.Cyan, "E", mutableStateOf("e@163")))
     return list
 }
 
