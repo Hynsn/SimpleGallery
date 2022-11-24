5a28db5dec6fa715750f77f7a1590e6fdd325e2c
Hynsn <hynson@163.com>
2022-11-24 22:59:42 +0800

    增加脚手架包裹Paging3刷新

--- app_kt/src/main/java/com/hynson/compose/paging/PagingActivity.kt
+++ app_kt/src/main/java/com/hynson/compose/paging/PagingActivity.kt
@@ -13,8 +13,12 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Menu
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.graphics.Color
@@ -29,66 +33,92 @@ import androidx.paging.compose.items
 import com.google.accompanist.swiperefresh.SwipeRefresh
 import com.google.accompanist.swiperefresh.rememberSwipeRefreshState
 import com.hynson.compose.scaffold.ScaffoldTest
+import kotlinx.coroutines.launch
 import java.io.IOException
 
 class PagingActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-//            Surface(
-//                modifier = Modifier.fillMaxSize(),
-//                color = MaterialTheme.colors.background
-//            ) {
-//                Greeting()
-//            }
-            ScaffoldTest()
+            Surface(
+                modifier = Modifier.fillMaxSize(),
+                color = MaterialTheme.colors.background
+            ) {
+                Greeting()
+            }
         }
     }
 }
 
 @Composable
 fun Greeting() {
+    val TAG = "加载状态"
     val mainViewmodel: PagingViewModel = viewModel()
     val data = mainViewmodel.getData().collectAsLazyPagingItems()
     val refreshState = rememberSwipeRefreshState(isRefreshing = false)
-    SwipeRefresh(state = refreshState, onRefresh = {
-        data.refresh()
-    }) {
-        LazyColumn() {
-            items(items = data) { item ->
-                Message(data = item)
-            }
 
-            data.apply {
-                when {
-                    // 加载更多：正在加载
-                    loadState.append is LoadState.Loading -> {
-                        item {
-                            LoadingItem()
-                        }
-                    }
-                    // 加载更多：出错
-                    loadState.append is LoadState.Error -> {
-                        item {
-                            ErrorItem() {
-                                data.retry()
-                            }
-                        }
-                    }
-                    // 刷新：无缓存数据时
-                    loadState.refresh is LoadState.Error && data.itemCount <= 0 -> {
-                        item {
-                            ErrorContent() {
-                                data.retry()
-                            }
-                        }
-                    }
-                    // 刷新：有缓存数据时，Toast提示
-                    loadState.refresh is LoadState.Error && data.itemCount > 0 -> {
-
-                    }
+    val scaffoldState = rememberScaffoldState()
+    val scope = rememberCoroutineScope()
+    var lastRefresh = true
+    Scaffold(scaffoldState = scaffoldState, topBar = {
+        TopAppBar(title = { Text("脚手架") },
+            navigationIcon = {
+                IconButton(onClick = {
+                    scope.launch { scaffoldState.drawerState.open() }
+                }) {
+                    Icon(Icons.Filled.Menu, contentDescription = null)
                 }
-            }
+            })
+    }, content = {
+        val paddingValues = it
+        SwipeRefresh(state = refreshState, onRefresh = {
+            data.refresh()
+        }) {
+            LazyColumn() {
+                items(items = data) { item ->
+                    Message(data = item)
+                }
+
+                data.run {
+                    when {
+                        // 加载更多：正在加载
+                        loadState.append is LoadState.Loading -> {
+                            item {
+                                LoadingItem()
+                            }
+                        }
+                        // 加载更多：出错
+                        loadState.append is LoadState.Error -> {
+                            item {
+                                ErrorItem() {
+                                    data.retry()
+                                }
+                            }
+                        }
+                        // 刷新：无缓存数据时
+                        loadState.refresh is LoadState.Error && data.itemCount <= 0 -> {
+                            item {
+                                ErrorContent() {
+                                    data.retry()
+                                }
+                            }
+                        }
+                        // 刷新：有缓存数据时，Toast提示
+                        loadState.refresh is LoadState.Error && data.itemCount > 0 -> {
+                            if(lastRefresh){
+                                lastRefresh = false
+                                scope.launch {
+                                    scaffoldState.snackbarHostState.showSnackbar("刷新失败")
+                                }
+                            }
+
+                        }
+                        loadState.refresh is LoadState.Loading && data.itemCount > 0 -> {
+                            lastRefresh = true
+                        }
+                    }
+                    Log.d(TAG, "${loadState} counter ${data.itemCount}")
+                }
 
 //            val TAG = "加载状态"
 //
@@ -104,8 +134,21 @@ fun Greeting() {
 //                    }
 //                }
 //            }
+            }
         }
-    }
+    },
+        snackbarHost = {
+            SnackbarHost(it) { data ->
+                Snackbar(
+                    snackbarData = data,
+                    backgroundColor = Color.Blue,
+                    contentColor = Color.White,
+                    shape = RoundedCornerShape(10.dp)
+                )
+            }
+        }
+    )
+
 }
 
 @Composable
